cb1b6b43a36cbaf14f0714ca9b1634fc9f1d8e72
scientificworld <scientificworld@126.com>
2022-08-04 23:20:52 +0800

    Add generate_newtype_template.py

---
+++ generate_newtype_template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import json
+j = {"lang": "", "name": "", "data": {}}
+with open('meta.json', 'r') as f:
+	x = json.loads(f.read())["需要翻译的键值"]
+for i in x:
+	j["data"][i] = ""
+with open('template.json', 'w') as f:
+	f.write(json.dumps(j, indent = 4))
