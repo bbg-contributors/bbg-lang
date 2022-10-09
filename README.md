6924be9153d766e6f38db474e1cb0289f1714172
Misaka13514 <Misaka13514@gmail.com>
2022-10-09 21:10:47 +0800

    chore: decentralized check

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ This repository contains the i18n data of bbg.
 
 ## 增加新翻译条目
 
-程序通过读取简体中文和英语的翻译文件来判断哪些条目需要翻译，若要增加新翻译条目，只需先在简体中文（```zh_CN.json```）或英语（```en_US.json```）的翻译文件中添加该条目的翻译，然后再在其它语言的翻译文件中添加该条目的翻译。现在无需在```meta.json```中手动添加需要翻译的键值名。
+程序通过读取简体中文和英语的翻译文件来判断哪些条目需要翻译，若要增加新翻译条目，只需先在简体中文（`zh_CN.json`）或英语（`en_US.json`）的翻译文件中添加该条目的翻译，然后再在其它语言的翻译文件中添加该条目的翻译。现在无需在 `meta.json` 中手动添加需要翻译的键值名。
 
 ## 检查已有语种的翻译进度
 
-```
-python ./check_translation_progress.py
+```sh
+python3 check_translation_progress.py --help
+python3 check_translation_progress.py --id zh_CN
+python3 check_translation_progress.py --id ja_JP --target-id en_US --verbose
+python3 check_translation_progress.py --id en_US --reverse
+python3 check_translation_progress.py --all
+python3 check_translation_progress.py --all --fix --sort
 ```
--- check_translation_progress.py
+++ check_translation_progress.py
@@ -1,132 +1,69 @@
 #!/usr/bin/python3
-"""
+'''
 Usage:
 $ python3 check_translation_progress.py --help
 $ python3 check_translation_progress.py --id zh_CN
-$ python3 check_translation_progress.py --id ja_JP --verbose
+$ python3 check_translation_progress.py --id ja_JP --target-id en_US --verbose
 $ python3 check_translation_progress.py --id en_US --reverse
 $ python3 check_translation_progress.py --all
 $ python3 check_translation_progress.py --all --fix --sort
-"""
+'''
 
 import argparse
+import os
 import json
 
 
-def is_valid_query(lang_id):
-    for i in range(len(lang_meta["名称与文件名之间的映射关系"])):
-        if lang_id == lang_meta["名称与文件名之间的映射关系"][i]['id']:
-            print(f"语言名称：{lang_meta['名称与文件名之间的映射关系'][i]['name']}")
-            print(f"语言id：{lang_meta['名称与文件名之间的映射关系'][i]['id']}\n")
-            return True
-    print(f"元文件中没有记录 id 为 {lang_id} ，所以检查失败。", )
-    print("如果这是你打算翻译的新语言，请先在元文件（meta.json）中加入有关该语言的信息。\n")
-    return False
+def is_valid(lang_data):
+    required_keys = ['LANG', 'LANG_NAME']
+    return all(key in lang_data for key in required_keys)
 
 
-def check_selected(lang_id):
-    if args.reverse:
-        print(f"\n现在使用 id 为 {lang_id} 的语言，反向检查 meta.json。\n")
+def check_selected(lang_data,
+                   lang_target,
+                   verbose=False,
+                   reverse=False,
+                   fix=False):
+    lang_id = lang_data['LANG']
+    target_id = lang_target['LANG']
+    if reverse:
+        print(f'\n现在使用 ID 为 {lang_id} 的语言，反向检查 {target_id}。')
+        lang_data, lang_target = lang_target, lang_data
+        lang_id = lang_data['LANG']
+        target_id = lang_target['LANG']
     else:
-        print(f"\n现在检查 id 为 {lang_id} 的语言的翻译进度。\n")
+        print(f'\n现在检查 ID 为 {lang_id} 的语言的翻译进度。')
 
-    if is_valid_query(lang_id):
-        with open(f"multi_language/{lang_id}.json", 'r') as f:
-            lang_current = json.load(f)
-
-        keysCurrentNumber = 0
-        keysTranslatedNumber = 0
-
-        if args.reverse:
-            keysTotalNumber = len(lang_current)
-            for key in lang_current:
-                if key in lang_meta["需要翻译的键值"]:
-                    keysCurrentNumber += 1
-                else:
-                    if args.verbose:
-                        print(f"{key} 不在 meta.json 中。")
+    translated_num = 0
+    for key in lang_target:
+        if key not in lang_data:
+            if verbose:
+                print(f'{key} 不在 {lang_id} 中。')
+            if fix:
+                lang_data[key] = lang_target[key]
+                if verbose:
+                    print(f'已将 {key} 的值设置为 {lang_target[key]}。')
         else:
-            keysTotalNumber = len(lang_meta["需要翻译的键值"])
-            for key in lang_meta["需要翻译的键值"]:
-                if key in lang_current:
-                    if not args.reverse and lang_current[key] == "":
-                        if args.verbose:
-                            print(f"{key} 没有翻译。")
-                    else:
-                        keysTranslatedNumber += 1
-                    keysCurrentNumber += 1
-                else:
-                    if args.verbose:
-                        print(f"{key} 不在 {lang_id}.json 中。")
+            translated_num += 1
 
-        if args.verbose:
-            if keysCurrentNumber != keysTotalNumber:
-                print()
-            elif not args.reverse and keysTranslatedNumber != keysTotalNumber:
-                print()
+    if translated_num != len(lang_target) and verbose:
+        print()
 
-        print(f"有效的键值数：{keysCurrentNumber}")
-        if not args.reverse:
-            print(f"已翻译的键值数：{keysTranslatedNumber}")
-        print(f"总键值数：{keysTotalNumber}\n")
+    print(f'目标语言共有 {len(lang_target)} 个条目，其中 {translated_num} 个已翻译。')
+    print(f'翻译进度为 {translated_num / len(lang_target) * 100:.2f}%。\n\n')
 
-        print(f"百分比：{keysCurrentNumber / keysTotalNumber * 100}%\n")
-
-        if keysCurrentNumber == keysTotalNumber:
-            return True
-        else:
-            return False
-
-
-def fix_selected(lang_id):
-    if args.reverse:
-        print(f"\n现在使用 id 为 {lang_id} 的语言，反向修复 meta.json。\n")
-    else:
-        print(f"\n现在修复 id 为 {lang_id} 的语言。\n")
-    if is_valid_query(lang_id):
-        with open('meta.json', 'r') as f:
-            lang_meta = json.load(f)
-        with open(f"multi_language/{lang_id}.json", 'r') as f:
-            lang_current = json.load(f)
-
-        if args.reverse:
-            for key in lang_current:
-                if key not in lang_meta["需要翻译的键值"]:
-                    lang_meta["需要翻译的键值"].append(key)
-                    if args.verbose:
-                        print(f"{key} 已添加到 meta.json 中。")
-            if args.sort:
-                lang_meta["需要翻译的键值"].sort()
-
-            with open('meta.json', 'w') as f:
-                json.dump(lang_meta,
-                          f,
-                          sort_keys=args.sort,
-                          ensure_ascii=False,
-                          indent=4)
-
-            print("已修复 meta.json \n")
-        else:
-            for key in lang_meta["需要翻译的键值"]:
-                if key not in lang_current:
-                    lang_current[key] = ""
-                    if args.verbose:
-                        print(f"{key} 已添加到 {lang_id}.json 中。")
-
-            with open(f"multi_language/{lang_id}.json", 'w') as f:
-                json.dump(lang_current,
-                          f,
-                          sort_keys=args.sort,
-                          ensure_ascii=False,
-                          indent=4)
-
-            print(f"已修复 id 为 {lang_id} 的语言。\n")
+    return lang_data
 
 
 parser = argparse.ArgumentParser(description='检查已有语种的翻译进度')
 parser.add_argument('--id',
                     type=str,
                     help='语言id',
+                    default=None,
+                    required=False)
+parser.add_argument('--target-id',
+                    type=str,
+                    help='目标语言id',
                     default=None,
                     required=False)
 parser.add_argument('-a', '--all', action='store_true', help='检查所有语言')
@@ -136,25 +73,52 @@ parser.add_argument('-f', '--fix', action='store_true', help='修复语言')
 parser.add_argument('-s', '--sort', action='store_true', help='修复时，按照键值排序')
 args = parser.parse_args()
 
-with open('meta.json', 'r') as f:
-    lang_meta = json.load(f)
+if args.id is None and not args.all:
+    print('请输入语言 ID。')
+    exit(1)
 
-if args.all:
-    for i in range(len(lang_meta["名称与文件名之间的映射关系"])):
-        lang_id = lang_meta["名称与文件名之间的映射关系"][i]['id']
-        lang_stat = check_selected(lang_id)
-        if args.fix:
-            if not lang_stat:
-                fix_selected(lang_id)
-            else:
-                print(f"id 为 {lang_id} 的语言无需修复。\n")
-else:
-    if args.id is None:
-        print("请输入语言id。")
+if args.target_id is None:
+    if args.id == 'zh_CN':
+        args.target_id = 'en_US'
     else:
-        lang_stat = check_selected(args.id)
-        if args.fix:
-            if not lang_stat:
-                fix_selected(args.id)
+        args.target_id = 'zh_CN'
+
+lang_target = None
+for i in os.listdir('multi_language'):
+    with open(f'multi_language/{i}', 'r') as f:
+        lang_data = json.load(f)
+    if is_valid(lang_data):
+        if lang_data['LANG'] == args.target_id:
+            lang_target = lang_data
+
+if lang_target is None:
+    print(f'没有找到 ID 为 {args.target_id} 的语言。')
+    exit(1)
+
+for i in os.listdir('multi_language'):
+    with open(f'multi_language/{i}', 'r') as f:
+        lang_data = json.load(f)
+    if is_valid(lang_data):
+        lang_name = lang_data['LANG_NAME']
+        lang_id = lang_data['LANG']
+        if args.all or lang_id == args.id:
+            if lang_id != args.target_id:
+                print(f'语言名称：{lang_name}')
+                print(f'语言 ID：{lang_id}')
+                if lang_id != args.target_id:
+                    lang_data = check_selected(lang_data, lang_target,
+                                               args.verbose, args.reverse,
+                                               args.fix)
+                    if args.fix:
+                        with open(f'multi_language/{i}', 'w') as f:
+                            json.dump(lang_data,
+                                      f,
+                                      sort_keys=args.sort,
+                                      ensure_ascii=False,
+                                      indent=4)
+                            print(f'已将 {lang_id} 的语言文件保存。')
             else:
-                print(f"id 为 {args.id} 的语言无需修复。\n")
+                print(f'跳过 ID 为 {lang_id} 的语言。\n\n')
+    else:
+        if args.all:
+            print(f'文件 {i} 不完整。\n\n')
